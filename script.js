36b8169d41a881e2e9ae43a6ff5690e84e9c15ad
MuhammadAhmad881076 <68360437+MuhammadAhmad881076@users.noreply.github.com>
2020-07-22 22:40:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+
+let btnliked = document.querySelector('#liked');
+let btnliked2 = document.querySelector('#liked2');
+let btnliked3 = document.querySelector('#liked3');
+let btnliked4 = document.querySelector('#liked4');
+let btnliked5 = document.querySelector('#liked5');
+let btnliked6 = document.querySelector('#liked6');
+let btnliked7 = document.querySelector('#liked7');
+let btnliked8 = document.querySelector('#liked8');
+let btnliked9 = document.querySelector('#liked9');
+let btnliked10 = document.querySelector('#liked10');
+let btnliked11 = document.querySelector('#liked11');
+let btnliked12 = document.querySelector('#liked12');
+let btnliked13 = document.querySelector('#liked13');
+let btnliked14 = document.querySelector('#liked14');
+let btnlikednew1 = document.querySelector('#likednew1');
+let btnlikednew2 = document.querySelector('#likednew2');
+let btnlikednew3 = document.querySelector('#likednew3');
+let btnlikednew4 = document.querySelector('#likednew4');
+let btnlikednew5 = document.querySelector('#likednew5');
+let btnlikednew6 = document.querySelector('#likednew6');
+let btnlikednew7 = document.querySelector('#likednew7');
+
+btnliked.addEventListener('dblclick' , () => btnliked.style.backgroundColor='#377ab7')
+btnliked.addEventListener('click' , () => btnliked.style.backgroundColor='cornsilk')
+
+btnliked2.addEventListener('dblclick' , () => btnliked2.style.backgroundColor='#377ab7')
+btnliked2.addEventListener('click' , () => btnliked2.style.backgroundColor='cornsilk')
+
+btnliked3.addEventListener('dblclick' , () => btnliked3.style.backgroundColor='#377ab7')
+btnliked3.addEventListener('click' , () => btnliked3.style.backgroundColor='cornsilk')
+
+btnliked4.addEventListener('dblclick' , () => btnliked4.style.backgroundColor='#377ab7')
+btnliked4.addEventListener('click' , () => btnliked4.style.backgroundColor='cornsilk')
+
+btnliked5.addEventListener('dblclick' , () => btnliked5.style.backgroundColor='#377ab7')
+btnliked5.addEventListener('click' , () => btnliked5.style.backgroundColor='cornsilk')
+
+btnliked6.addEventListener('dblclick' , () => btnliked6.style.backgroundColor='#377ab7')
+btnliked6.addEventListener('click' , () => btnliked6.style.backgroundColor='cornsilk')
+
+btnliked7.addEventListener('dblclick' , () => btnliked7.style.backgroundColor='#377ab7')
+btnliked7.addEventListener('click' , () => btnliked7.style.backgroundColor='cornsilk')
+
+btnliked8.addEventListener('dblclick' , () => btnliked8.style.backgroundColor='#377ab7')
+btnliked8.addEventListener('click' , () => btnliked8.style.backgroundColor='cornsilk')
+
+btnliked9.addEventListener('dblclick' , () => btnliked9.style.backgroundColor='#377ab7')
+btnliked9.addEventListener('click' , () => btnliked9.style.backgroundColor='cornsilk')
+
+btnliked10.addEventListener('dblclick' , () => btnliked10.style.backgroundColor='#377ab7')
+btnliked10.addEventListener('click' , () => btnliked10.style.backgroundColor='cornsilk')
+
+btnliked11.addEventListener('dblclick' , () => btnliked11.style.backgroundColor='#377ab7')
+btnliked11.addEventListener('click' , () => btnliked11.style.backgroundColor='cornsilk')
+
+btnliked12.addEventListener('dblclick' , () => btnliked12.style.backgroundColor='#377ab7')
+btnliked12.addEventListener('click' , () => btnliked12.style.backgroundColor='cornsilk')
+
+btnliked13.addEventListener('dblclick' , () => btnliked13.style.backgroundColor='#377ab7')
+btnliked13.addEventListener('click' , () => btnliked13.style.backgroundColor='cornsilk')
+
+btnliked14.addEventListener('dblclick' , () => btnliked14.style.backgroundColor='#377ab7')
+btnliked14.addEventListener('click' , () => btnliked14.style.backgroundColor='cornsilk')
+
+btnlikednew1.addEventListener('dblclick' , () => btnlikednew1.style.backgroundColor='#377ab7')
+btnlikednew1.addEventListener('click' , () => btnlikednew1.style.backgroundColor='cornsilk')
+
+btnlikednew2.addEventListener('dblclick' , () => btnlikednew2.style.backgroundColor='#377ab7')
+btnlikednew2.addEventListener('click' , () => btnlikednew2.style.backgroundColor='cornsilk')
+
+btnlikednew3.addEventListener('dblclick' , () => btnlikednew3.style.backgroundColor='#377ab7')
+btnlikednew3.addEventListener('click' , () => btnlikednew3.style.backgroundColor='cornsilk')
+
+btnlikednew4.addEventListener('dblclick' , () => btnlikednew4.style.backgroundColor='#377ab7')
+btnlikednew4.addEventListener('click' , () => btnlikednew4.style.backgroundColor='cornsilk')
+
+btnlikednew5.addEventListener('dblclick' , () => btnlikednew5.style.backgroundColor='#377ab7')
+btnlikednew5.addEventListener('click' , () => btnlikednew5.style.backgroundColor='cornsilk')
+
+btnlikednew6.addEventListener('dblclick' , () => btnlikednew6.style.backgroundColor='#377ab7')
+btnlikednew6.addEventListener('click' , () => btnlikednew6.style.backgroundColor='cornsilk')
+
+btnlikednew7.addEventListener('dblclick' , () => btnlikednew7.style.backgroundColor='#377ab7')
+btnlikednew7.addEventListener('click' , () => btnlikednew7.style.backgroundColor='cornsilk')
+
